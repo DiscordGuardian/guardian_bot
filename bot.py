230626b6ab636266fbe2ca43ef1ca6f8c3e069ea
Epic <admin@itzepic.net>
2020-05-11 14:27:50 +0200

    Guardian is alive

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord import ext
+
+bot = discord.Client("guardian")
+Bot = ext.commands.Bot("////")
+
+async def nope(self: int):
+  for bot in Bot.members:
+    m = await bot.send("Thonk db corrupted sowwie")
+    await m.delete() # haha yes very funny")
+def e(e):
+  return e
+async def makeCommands(name: int, amoutn: str):
+  return e(e(nope))
+  
+client.run("epicwashereowo")
